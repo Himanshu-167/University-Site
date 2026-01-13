11f26bfa41b15ca52dcf2576ab74778f6ec48073
Himanshu-167 <himanshudahiya167@gmail.com>
2026-01-13 12:54:47 +0530

    build to docs for github pages

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   base: "/University-Site/",
 
+  build: {
+    outDir: "docs",   // 👈 THIS is the key fix
+    emptyOutDir: true
+  },
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
