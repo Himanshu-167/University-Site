30f1f9240e5c61b23e63c3a95d265cbb823de50e
Himanshu-167 <himanshudahiya167@gmail.com>
2026-01-22 20:20:32 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/University-Site/",
-
   build: {
-    outDir: "docs",   // 👈 THIS is the key fix
-    emptyOutDir: true
+    outDir: "docs",
   },
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  plugins: [react()],
+});
