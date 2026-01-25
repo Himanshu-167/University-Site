09ce14ce2debd9b27640f83e086620062ede26c1
Himanshu-167 <himanshudahiya167@gmail.com>
2026-01-25 20:16:22 +0530

    Replace BrowserRouter with HashRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
@@ -17,17 +17,13 @@ const App = () => (
     <TooltipProvider>
       <UserProvider>
         <Toaster />
-        <BrowserRouter basename="/University-Site/">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/attendance" element={<Attendance />} />
-            <Route path="/fees" element={<Fees />} />
-            <Route path="/communication" element={<Communication />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <HashRouter>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</HashRouter>
       </UserProvider>
     </TooltipProvider>
   </QueryClientProvider>
